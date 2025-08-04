4b2db1aa90a4b07dbd9b05a0bd1d420ad499a6cc
Enrisen Tzib <enrisentzib@gmail.com>
2025-08-03 19:02:09 -0600

    connected to supabase

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,8 +1,54 @@
+import supabase from '../supabase-client'
+import { useState } from 'react'
+import { useEffect } from 'react'
 
 export default function Search() {
+
+  const [cptCodes, setCptCodes] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
+
+  // useEffect(() => {
+  //   fetchCPTCodes();
+  // }, []);
+
+  const fetchCPTCodes = async () => {
+    const { data, error } = await supabase.from('cpt_code_v3').select('SELECT code, long_description, FROM cpt_code_v3 WHERE column_name = {searchTerm};');
+    if (error) {
+      console.error('Error fetching CPT codes:', error);
+    } else {
+      setCptCodes(data);
+    }
+  };
+
+
   return (
-    <div className="search-container">
-      <textarea id="cptSearch" placeholder="Enter CPT codes (e.g., 97110, 97140)" autocomplete="off"></textarea>
-    </div>
-  )
+  <div className="search-container">
+    <textarea
+      id="cptSearch"
+      placeholder="Enter CPT codes (e.g., 97110, 97140)"
+      autoComplete="off"
+    ></textarea>
+    <button onClick={fetchCPTCodes}>
+      Fetch Codes
+    </button>
+    <div id="results-wrapper">
+            <div id="resultsContainer">
+                <button id="copyButton" className="icon-button" title="Copy Results">
+
+                </button>
+                <p class="placeholder">Results will appear here.</p>
+                <ul>
+      {cptCodes.map((code) => (
+        <li key={code.id}>
+          <p>{code.code}</p>
+          <p>{code.short_description}</p>
+          <p>{code.long_description}</p>
+        </li>
+      ))}
+    </ul>
+            </div>
+        </div>
+  </div>
+)
+
 }
